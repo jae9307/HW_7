2370142dcb712869018ad3dd82fb1af6fd28392c
Daniel S Rigoglioso <dsr5964@g.rit.edu>
2025-04-12 18:09:24 -0400

    Dropped the ID column from data frame

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import numpy as np
 
 def pca():
     data_frame = pd.read_csv("HW_CLUSTERING_SHOPPING_CART_v2245a.csv")
+    data_frame = data_frame.drop('ID', axis=1)
 
     covariance = pd.DataFrame.cov(data_frame)
+    print(covariance.to_string())
 
     eigenvalues, eigenvectors = np.linalg.eig(covariance)
-    print(np.matrix(eigenvectors))
-    print(np.matrix(eigenvalues))
+    print("Eigenvectors ", np.matrix(eigenvectors))
+    print("Eigenvalues ", np.matrix(eigenvalues))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
